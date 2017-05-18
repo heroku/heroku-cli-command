46653e218fdf705bead02a28bd8c328dba0470ed
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-05-17 18:28:45 -0700

    Add --team flag

---
+++ src/flags/team.js
@@ -0,0 +1,19 @@
+// @flow
+
+import {type Flag} from 'cli-engine-command/lib/flags'
+
+type Options = $Shape<Flag<string>>
+export default function TeamFlag (options: Options = {}, env: typeof process.env = process.env): Flag<string> {
+  const envTeam = env.HEROKU_TEAM || env.HEROKU_ORGANIZATION
+  const defaultOptions: Options = {
+    char: 't',
+    description: 'team to use',
+    default: () => envTeam,
+    parse: (input) => {
+      if (input) return input
+      if (envTeam) return envTeam
+      if (options.required) throw new Error('No team specified')
+    }
+  }
+  return Object.assign(defaultOptions, options)
+}
---
+++ src/flags/team.test.js
@@ -0,0 +1,83 @@
+// @flow
+
+import Command from 'cli-engine-command'
+import TeamFlag from './team'
+
+describe('required', () => {
+  class TeamCommand extends Command {
+    static flags = {team: TeamFlag({required: true})}
+    team: string
+    async run () {
+      this.team = this.flags.team
+    }
+  }
+
+  test('has an team', async () => {
+    const cmd = await TeamCommand.mock('--team', 'myteam')
+    expect(cmd.team).toEqual('myteam')
+  })
+
+  test('-t', async () => {
+    const cmd = await TeamCommand.mock('-t', 'myteam')
+    expect(cmd.team).toEqual('myteam')
+  })
+
+  test('errors with no team', async () => {
+    expect.assertions(1)
+    try {
+      await TeamCommand.mock()
+    } catch (err) {
+      expect(err.message).toContain('No team specified')
+    }
+  })
+})
+
+describe('optional', () => {
+  class TeamCommand extends Command {
+    static flags = {team: TeamFlag()}
+    team: ?string
+    async run () {
+      this.team = this.flags.team
+    }
+  }
+
+  test('--team', async () => {
+    const cmd = await TeamCommand.mock('--team', 'myteam')
+    expect(cmd.team).toEqual('myteam')
+  })
+
+  test('-t', async () => {
+    const cmd = await TeamCommand.mock('-t', 'myteam')
+    expect(cmd.team).toEqual('myteam')
+  })
+
+  test('reads HEROKU_ORGANIZATION as a backup', async () => {
+    class TeamCommand extends Command {
+      static flags = {team: TeamFlag({}, {'HEROKU_ORGANIZATION': 'myteam'})}
+      team: ?string
+      async run () {
+        this.team = this.flags.team
+      }
+    }
+
+    const cmd = await TeamCommand.mock()
+    expect(cmd.team).toEqual('myteam')
+  })
+
+  test('reads HEROKU_TEAM', async () => {
+    class TeamCommand extends Command {
+      static flags = {team: TeamFlag({}, {'HEROKU_TEAM': 'myteam'})}
+      team: ?string
+      async run () {
+        this.team = this.flags.team
+      }
+    }
+
+    const cmd = await TeamCommand.mock()
+    expect(cmd.team).toEqual('myteam')
+  })
+
+  test('does not error when team is not specified', async () => {
+    await TeamCommand.mock()
+  })
+})
