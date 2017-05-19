43f69aa98f06925b58e895e5836bbc3a9c6e09c0
RasPhilCo <capt.pn@gmail.com>
2017-05-19 16:01:43 -0700

    Add flags.org (#7)

--- src/flags/team.js
+++ src/flags/team.js
@@ -8,9 +8,10 @@ export default function TeamFlag (options: Options = {}, env: typeof process.env
   const defaultOptions: Options = {
     char: 't',
     description: 'team to use',
-    parse: (input) => {
+    parse: (input, cmd) => {
       if (input) return input
       if (envTeam) return envTeam
+      if (cmd && cmd.flags.org) return cmd.flags.org
       if (options.required) throw new Error('No team specified')
     }
   }
