96f21ebf84c72578afdfacd77b95c3e4e67b2972
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-05-19 10:28:15 -0700

    added heroku-client (#4)
    * added heroku-client
    
    * added test for heroku clients

--- src/command.js
+++ src/command.js
@@ -1,12 +1,34 @@
 // @flow
 
 import Command from 'cli-engine-command'
+import APIClient from './api_client'
 
 export default class HerokuCommand extends Command {
   app: ?string
+  _heroku: APIClient
+  _legacyHerokuClient: any
 
   async init () {
     await super.init()
     if (this.flags.app) this.app = this.flags.app
+  }
+
+  get heroku (): APIClient {
+    if (this._heroku) return this._heroku
+    this._heroku = new APIClient(this.out)
+    return this._heroku
+  }
+  get legacyHerokuClient (): any {
+    if (this._legacyHerokuClient) return this._legacyHerokuClient
+    const HerokuClient = require('heroku-client')
+    let options = {
+      userAgent: this.heroku.requestOptions.headers['user-agent'],
+      debug: this.config.debug,
+      token: this.heroku.auth,
+      host: `${this.heroku.requestOptions.protocol || 'https:'}//${this.heroku.requestOptions.host || 'api.heroku.com'}`
+    }
+
+    this._legacyHerokuClient = new HerokuClient(options)
+    return this._heroku
   }
 }
