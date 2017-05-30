7932eb643012aff8768febeaafbfa724bef2a30c
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-05-30 15:42:26 -0700

    Parse input -> env -> org flag

--- src/flags/team.js
+++ src/flags/team.js
@@ -4,7 +4,7 @@ import {type Flag} from 'cli-engine-command/lib/flags'
 
 type Options = $Shape<Flag<string>>
 export default function TeamFlag (options: Options = {}, env: typeof process.env = process.env): Flag<string> {
-  const envTeam = env.HEROKU_TEAM || env.HEROKU_ORGANIZATION
+  const envTeam = env.HEROKU_TEAM
   const defaultOptions: Options = {
     char: 't',
     description: 'team to use',
