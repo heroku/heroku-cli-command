681710894b934e3c5561852167c89d58f00ebfa0
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-05-30 15:49:24 -0700

    update

--- src/flags/team.js
+++ src/flags/team.js
@@ -12,6 +12,7 @@ export default function TeamFlag (options: Options = {}, env: typeof process.env
       if (input) return input
       if (envTeam) return envTeam
       if (cmd && cmd.flags.org) return cmd.flags.org
+      if (env.HEROKU_ORGANIZATION) return env.HEROKU_ORGANIZATION
       if (options.required) throw new Error('No team specified')
     }
   }
