631dce2ebdc5a0056ba32f758803030a1a4c51c9
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-05-31 14:21:39 -0700

    updates

--- src/flags/org.js
+++ src/flags/org.js
@@ -4,13 +4,12 @@ import {merge, type Flag} from 'cli-engine-command/lib/flags'
 
 type Options = $Shape<Flag<string>>
 export default function OrgFlag (options: Options = {}, env: typeof process.env = process.env): Flag<string> {
-  const envTeam = env.HEROKU_ORGANIZATION
   const defaultOptions: Options = {
     char: 'o',
     hidden: true,
     parse: (input, cmd) => {
       if (input) return input
-      if (envTeam) return envTeam
+      if (env.HEROKU_ORGANIZATION) return env.HEROKU_ORGANIZATION
       if (options.required) throw new Error('No org specified')
     }
   }
--- src/flags/team.js
+++ src/flags/team.js
@@ -4,13 +4,12 @@ import {merge, type Flag} from 'cli-engine-command/lib/flags'
 
 type Options = $Shape<Flag<string>>
 export default function TeamFlag (options: Options = {}, env: typeof process.env = process.env): Flag<string> {
-  const envTeam = env.HEROKU_TEAM
   const defaultOptions: Options = {
     char: 't',
     description: 'team to use',
     parse: (input, cmd) => {
       if (input) return input
-      if (envTeam) return envTeam
+      if (env.HEROKU_TEAM) return env.HEROKU_TEAM
       if (cmd && cmd.flags.org) return cmd.flags.org
       if (env.HEROKU_ORGANIZATION) return env.HEROKU_ORGANIZATION
       if (options.required) throw new Error('No team specified')
