10403774162e41985ea84455a38d631ef3fcf01c
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-06-01 20:00:18 -0700

    add base flags

--- src/flags/index.js
+++ src/flags/index.js
@@ -1,9 +1,14 @@
 // @flow
 
+import {flags as baseFlags} from 'cli-engine-command'
 import team from './team'
 import org from './org'
+
 export const flags = {
+  org,
   team,
-  org
+  boolean: baseFlags.boolean,
+  number: baseFlags.number,
+  string: baseFlags.string
 }
 export {default as merge} from 'lodash.merge'
