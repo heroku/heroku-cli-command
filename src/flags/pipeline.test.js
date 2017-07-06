1f0eeddb07256fad0c5558335464238952502a9f
Matthew O <moriger@mykolab.com>
2017-07-06 14:34:34 -0500

    Feature/pipeline flag (#15)
    * add pipeline flag
    
    * remove some tests

---
+++ src/flags/pipeline.test.js
@@ -0,0 +1,58 @@
+// @flow
+
+import Command from 'cli-engine-command'
+import PipelineFlag from './pipeline'
+
+describe('required', () => {
+  class PipelineCommand extends Command {
+    static flags = {pipeline: PipelineFlag({required: true})}
+    pipeline: string
+
+    async run () {
+      this.pipeline = this.flags.pipeline
+    }
+  }
+
+  test('has a pipeline', async () => {
+    const cmd = await PipelineCommand.mock('--pipeline', 'mypipeline')
+    expect(cmd.pipeline).toEqual('mypipeline')
+  })
+
+  test('errors with no pipeline', async () => {
+    expect.assertions(1)
+    try {
+      await PipelineCommand.mock()
+    } catch (err) {
+      expect(err.message).toContain('No pipeline specified')
+    }
+  })
+})
+
+describe('optional', () => {
+  class PipelineCommand extends Command {
+    static flags = {pipeline: PipelineFlag()}
+    pipeline: ?string
+
+    async run () {
+      this.pipeline = this.flags.pipeline
+    }
+  }
+
+  test('--pipeline', async () => {
+    const cmd = await PipelineCommand.mock('--pipeline', 'mypipeline')
+    expect(cmd.pipeline).toEqual('mypipeline')
+  })
+
+  test('-p', async () => {
+    const cmd = await PipelineCommand.mock('-p', 'mypipeline')
+    expect(cmd.pipeline).toEqual('mypipeline')
+  })
+
+  test('is not hidden by default', async () => {
+    expect(PipelineCommand.flags.pipeline.hidden).toBeFalsy()
+  })
+
+  test('does not error when pipeline is not specified', async () => {
+    await PipelineCommand.mock()
+  })
+})
