4b70da6b802b039ebf6515a1eba82efcd9346a8f
Matthew Origer <moriger@heroku.com>
2017-07-06 13:56:32 -0500

    remove some tests

--- src/flags/pipeline.test.js
+++ src/flags/pipeline.test.js
@@ -3,33 +3,6 @@
 import Command from 'cli-engine-command'
 import PipelineFlag from './pipeline'
 
-it('has a short form of "p"', () => {
-  const pflag: PipelineFlag = new PipelineFlag()
-  expect(pflag.char).toBe('p')
-})
-
-it('is not hidden', () => {
-  const pflag: PipelineFlag = new PipelineFlag()
-  expect(pflag.hidden).toBeFalsy()
-})
-
-it('returns the input when it is given', () => {
-  const pflag: PipelineFlag = new PipelineFlag()
-  expect(pflag.parse('input', {some: 'command'})).toEqual('input')
-})
-
-it('throws an error when it is required but not given', () => {
-  const pflag: PipelineFlag = new PipelineFlag({required: true})
-  let thrown = false
-  try {
-    pflag.parse(undefined, {some: 'command'})
-  } catch (e) {
-    thrown = true
-    expect(e.message).toEqual('No pipeline specified')
-  }
-  expect(thrown).toEqual(true)
-})
-
 describe('required', () => {
   class PipelineCommand extends Command {
     static flags = {pipeline: PipelineFlag({required: true})}
