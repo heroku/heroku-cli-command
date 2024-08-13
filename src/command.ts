0d2570178669b6c84c8157b8ac2eca1c99e43481
Zane Whitfield <zwhitfield@salesforce.com>
2024-08-13 11:19:21 -0700

    Update deprecation message
    Remove portion of message to use in command so that command specific syntax is not shared amongst other commands

--- src/command.ts
+++ src/command.ts
@@ -59,7 +59,7 @@ export abstract class Command extends Base {
         const nonExistentFlagsWithValues = {...parsed}
 
         if (nonExistentFlags && nonExistentFlags.length > 0) {
-          this.warn(`You’re using a deprecated syntax with the [${nonExistentFlags}] flag.\nAdd a '--' (end of options) separator before the flags you’re passing through.\nSee https://devcenter.heroku.com/changelog-items/2925 for more info.`)
+          this.warn(`You’re using a deprecated syntax with the [${nonExistentFlags}] flag.\nAdd a '--' (end of options) separator before the flags you’re passing through.`)
           for (const flag of nonExistentFlags) {
             const key = flag.replace('--', '')
             delete parsed[key]
