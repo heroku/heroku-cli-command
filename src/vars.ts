c710d4278cb429635b638083dbabf1db6420a438
Eric <eblack@salesforce.com>
2025-08-12 09:52:36 -0700

    W-19264912 feat: add domain name validation for outgoing requests (#218)
    * add domain name validation to outgoing requests
    
    * reword
    
    * fix domain check to include localhost
    
    * use ux.warn for warning message
    
    * Apply suggestions from code review
    
    Co-authored-by: Justin Wilaby <jwilaby@gmail.com>
    Signed-off-by: Eric <eablack@gmail.com>
    
    * remove unnecessary as const
    
    ---------
    
    Signed-off-by: Eric <eablack@gmail.com>
    Co-authored-by: Justin Wilaby <jwilaby@gmail.com>

--- src/vars.ts
+++ src/vars.ts
@@ -1,4 +1,7 @@
+import {ux} from '@oclif/core'
 import * as url from 'node:url'
+
+import {ALLOWED_HEROKU_DOMAINS, LOCALHOST_DOMAINS} from './api-client.js'
 
 export class Vars {
   get apiHost(): string {
@@ -41,7 +44,14 @@ export class Vars {
   }
 
   get host(): string {
-    return this.envHost || 'heroku.com'
+    const {envHost} = this
+
+    if (envHost && !this.isValidHerokuHost(envHost)) {
+      ux.warn(`Invalid HEROKU_HOST '${envHost}' - using default`)
+      return 'heroku.com'
+    }
+
+    return envHost || 'heroku.com'
   }
 
   get httpGitHost(): string {
@@ -61,6 +71,13 @@ export class Vars {
     return process.env.HEROKU_CLOUD === 'staging'
       ? 'https://particleboard-staging-cloud.herokuapp.com'
       : 'https://particleboard.heroku.com'
+  }
+
+  private isValidHerokuHost(host: string): boolean {
+    // Remove protocol if present
+    const cleanHost = host.replace(/^https?:\/\//, '')
+
+    return ALLOWED_HEROKU_DOMAINS.some(domain => cleanHost.endsWith(`.${domain}`)) || LOCALHOST_DOMAINS.some(domain => cleanHost.includes(domain))
   }
 }
 
