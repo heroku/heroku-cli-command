a12b26aec61097914be979c3d7b4823ecc6d133d
Erika Wallace <erika.wallace@salesforce.com>
2026-04-13 16:32:57 -0400

    remove unused imports

--- test/mocha-hooks.ts
+++ test/mocha-hooks.ts
@@ -1,6 +1,3 @@
-import nock from 'nock'
-import {resolve} from 'node:path'
-
 import {stubCredentialManager} from './helpers/credential-manager-stub.js'
 
 /** Mirrors @heroku-cli/test-utils initCliTest (not a devDependency here to avoid cycle with this package). */
