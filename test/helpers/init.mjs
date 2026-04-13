4ec1ca247ddaad0bb095e66295a3d6bd4d8c000c
Erika Wallace <erika.wallace@salesforce.com>
2026-04-13 16:53:30 -0400

    update globalThis assignment

--- test/helpers/init.mjs
+++ test/helpers/init.mjs
@@ -3,7 +3,7 @@ import {resolve} from 'path'
 
 process.env.NODE_ENV = 'test'
 process.env.TS_NODE_PROJECT = resolve('test/tsconfig.json')
-Object.assign(globalThis, {columns: '120'})
+globalThis.columns = '120'
 nock.disableNetConnect()
 if (process.env.ENABLE_NET_CONNECT === 'true') {
   nock.enableNetConnect()
