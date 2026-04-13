2b7bc8388b1e9b592c10ce390529f166d9158469
Erika Wallace <erika.wallace@salesforce.com>
2026-04-13 16:20:33 -0400

    update testing config

---
+++ test/helpers/init.mjs
@@ -0,0 +1,9 @@
+import nock from 'nock'
+
+process.env.NODE_ENV = 'test'
+process.env.TS_NODE_PROJECT = resolve('test/tsconfig.json')
+Object.assign(globalThis, {columns: '120'})
+nock.disableNetConnect()
+if (process.env.ENABLE_NET_CONNECT === 'true') {
+  nock.enableNetConnect()
+}
